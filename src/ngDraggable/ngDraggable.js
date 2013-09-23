cc079272d21462fea049027295f5afe7101d6bc8
tboz203 <tboz203@gmail.com>
2013-09-23 15:00:25 -0500

    Prettifying source (tabs && trailing spaces)

--- src/ngDraggable/ngDraggable.js
+++ src/ngDraggable/ngDraggable.js
@@ -1,4 +1,4 @@
- /**
+/**
  * Draggable directive, allow to drag any element
  * targeted by this directive.
  *
@@ -10,124 +10,124 @@
  */
 (function () {
     "use strict";
-	
-	/**
-	 * @Class
-	 */ 
+
+    /**
+     * @Class
+     */
     var Draggable = function($scope,$element,$attrs){
 
-    	var offset = {},
-    		body   = $('body'),
-    		isDragging = false;
+        var offset = {},
+            body   = $('body'),
+            isDragging = false;
 
-    	/**
-	     * Initialize Draggable to default settings
-	     */  
-    	function init(){
-    		$element.css("position","absolute");
+        /**
+         * Initialize Draggable to default settings
+         */
+        function init(){
+            $element.css("position","absolute");
 
-    		if ("ontouchstart" in document.documentElement){
-    			$element.bind('touchstart.ngDraggable',startDrag);
-    			body.bind('touchend.ngDraggable',stopDrag);
-    			body.bind('touchmove.ngDraggable',drag);
-    		}else{
-    			$element.bind('mousedown.ngDraggable',startDrag);
-    			body.bind('mouseup.ngDraggable',stopDrag);
-    			body.bind('mousemove.ngDraggable',drag);
-    		}
-    		
-    		$scope.$on('destroy',destroy);
-    	};
+            if ("ontouchstart" in document.documentElement){
+                $element.bind('touchstart.ngDraggable',startDrag);
+                body.bind('touchend.ngDraggable',stopDrag);
+                body.bind('touchmove.ngDraggable',drag);
+            }else{
+                $element.bind('mousedown.ngDraggable',startDrag);
+                body.bind('mouseup.ngDraggable',stopDrag);
+                body.bind('mousemove.ngDraggable',drag);
+            }
+
+            $scope.$on('destroy',destroy);
+        };
 
 
-    	/**
-	     * Triggered when a user start moving his mouse
-	     * or his finger on mobile application.
-		 *
-	     * @param event
-	     */ 
-    	function drag(event){
-    		if(isDragging){
-    			var position = {};
+        /**
+         * Triggered when a user start moving his mouse
+         * or his finger on mobile application.
+         *
+         * @param event
+         */
+        function drag(event){
+            if(isDragging){
+                var position = {};
 
-    			if(event.type==="mousemove"){
+                if(event.type==="mousemove"){
                     position.x = Math.floor(event.pageX-offset.x);
-    				position.y = Math.floor(event.pageY-offset.y);
-    				
-    			}else{
-    				var orig = event.originalEvent;  
-				    position.x = Math.floor(orig.changedTouches[0].pageX - offset.x); 
-				    position.y = Math.floor(orig.changedTouches[0].pageY - offset.y);  
-				}
+                    position.y = Math.floor(event.pageY-offset.y);
 
-    			$element.css("left",position.x);
-				$element.css("top",position.y);
-			}
-    	}
+                }else{
+                    var orig = event.originalEvent;
+                    position.x = Math.floor(orig.changedTouches[0].pageX - offset.x);
+                    position.y = Math.floor(orig.changedTouches[0].pageY - offset.y);
+                }
+
+                $element.css("left",position.x);
+                $element.css("top",position.y);
+            }
+        }
 
 
-    	/**
-    	 * Triggered when a user click or start touching the element.
-		 *
-	     * @param event
-	     */ 
-    	function startDrag(event){
-    		isDragging = true;
+        /**
+         * Triggered when a user click or start touching the element.
+         *
+         * @param event
+         */
+        function startDrag(event){
+            isDragging = true;
 
-    		if(event.type==="mousedown"){
+            if(event.type==="mousedown"){
                 var localoffsetX = $element.offset().left,
                     localoffsetY = $element.offset().top,
                     localx = Math.floor(event.pageX-localoffsetX),
                     localy = Math.floor(event.pageY-localoffsetY);
-              	offset.x = localx;
-	    		offset.y = localy;
+                offset.x = localx;
+                offset.y = localy;
            }else{
-    			var orig = event.originalEvent;  
-			    var pos  = $(this).position();  
-			    offset.x = Math.floor(orig.changedTouches[0].pageX - pos.left);
-			    offset.y = Math.floor(orig.changedTouches[0].pageY - pos.top);
-    		}
+                var orig = event.originalEvent;
+                var pos  = $(this).position();
+                offset.x = Math.floor(orig.changedTouches[0].pageX - pos.left);
+                offset.y = Math.floor(orig.changedTouches[0].pageY - pos.top);
+            }
 
             $scope.$emit("$startDrag");
-    		event.preventDefault();
-    	}
+            event.preventDefault();
+        }
 
 
-    	/**
-    	 * Triggered when a user remove his finger from the screen.
-		 *
-	     * @param event
-	     */ 
-    	function stopDrag(event){
-    		isDragging = false;
+        /**
+         * Triggered when a user remove his finger from the screen.
+         *
+         * @param event
+         */
+        function stopDrag(event){
+            isDragging = false;
             offset = {};
-    		$scope.$emit("$stopDrag");
-    	}
+            $scope.$emit("$stopDrag");
+        }
 
 
-    	/**
-    	 * Cleanup all listeners before destroying this directive.
-	     */ 
-    	function destroy(){
-			$element.unbind('mousedown.ngDraggable',startDrag);
-    		$element.unbind('mousestart.ngDraggable',startDrag);
-    		body.unbind('mouseup.ngDraggable',stopDrag);
-    		body.unbind('touchend.ngDraggable',stopDrag);
-    		body.unbind('mousemove.ngDraggable',drag);
-    		body.unbind('touchmove.ngDraggable',drag);
-    	}
+        /**
+         * Cleanup all listeners before destroying this directive.
+         */
+        function destroy(){
+            $element.unbind('mousedown.ngDraggable',startDrag);
+            $element.unbind('mousestart.ngDraggable',startDrag);
+            body.unbind('mouseup.ngDraggable',stopDrag);
+            body.unbind('touchend.ngDraggable',stopDrag);
+            body.unbind('mousemove.ngDraggable',drag);
+            body.unbind('touchmove.ngDraggable',drag);
+        }
 
 
-		init();
+        init();
     };
 
     angular.module('ngDraggable',[])
-    	.directive('ngDraggable',function () {
-    		return {
-			    restrict: 'A',
-			    link: function ($scope, $element, $attrs) {
-			    	new Draggable($scope, $element, $attrs);
-				}
-			}	
-	});
+        .directive('ngDraggable',function () {
+            return {
+                restrict: 'A',
+                link: function ($scope, $element, $attrs) {
+                    new Draggable($scope, $element, $attrs);
+                }
+            }
+    });
 })();
